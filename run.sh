f71b5901db1bac8a9957527241939dfe442c8905
Joe Hanink <jhanink@gmail.com>
2016-07-21 18:19:52 -0700

    update run script

--- run.sh
+++ run.sh
@@ -5,11 +5,13 @@ cd $_DIR
 cat $(ls -a | grep -E '^\d') > ../../combined.csv
 cd $_PWD
 
+_NUM_ROWS=$(wc -l < combined.csv)
+_ETA=$(( _NUM_ROWS / 18700 ))
+echo "" && echo "Processing ${_NUM_ROWS//[[:blank:]]/} records"
+echo "Estimated run time: Less than ${_ETA} seconds"
 while :;do echo -n .;sleep 1;done &
 trap "kill $!" EXIT  #Die with parent if we die prematurely
 time node index.js | python -m json.tool > combined.json
 open -a /Applications/Google\ Chrome.app ./combined.json
 kill $! && trap " " EXIT
-
-
 
