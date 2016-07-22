893e98ae9a0830cdcada67b3e92a171e5e93d975
Joe Hanink <jhanink@gmail.com>
2016-07-21 18:57:51 -0700

    update script with 60 second breaks

--- run.sh
+++ run.sh
@@ -9,7 +9,16 @@ _NUM_ROWS=$(wc -l < combined.csv)
 _ETA=$(( _NUM_ROWS / 18700 ))
 echo "" && echo "Processing ${_NUM_ROWS//[[:blank:]]/} records"
 echo "Estimated run time: Less than ${_ETA} seconds"
-while :;do echo -n .;sleep 1;done &
+_COUNT=0
+while :;
+do
+  if [ $_COUNT -ne 0 ]  && [ $(( _COUNT % 60)) ==  0 ]
+    then
+      echo ""
+  fi;
+  echo -n .;sleep 1;
+  _COUNT=$((_COUNT + 1));
+done &
 trap "kill $!" EXIT  #Die with parent if we die prematurely
 time node index.js | python -m json.tool > combined.json
 open -a /Applications/Google\ Chrome.app ./combined.json
